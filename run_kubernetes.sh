621d924ba96d056152e19d653a62d47d89585995
IdlCM4 <irinadelacorte@gmail.com>
2023-10-05 13:09:24 +0200

    Added missing commit

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,23 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=idlcm4/housingprice
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+r=$(kubectl get pod housingprice 2> /dev/null;echo $?)
+if [ "_$r" == "_1" ]
+ then
+    kubectl run housingprice \
+    --image=$dockerpath \
+    --image-pull-policy="Always" \
+    --overrides='{"apiVersion": "v1", "spec":{"imagePullSecrets": [{"name": "regcred"}]}}'
+fi
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward housingprice 8080:80
